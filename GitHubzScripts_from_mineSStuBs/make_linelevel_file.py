2cf5694d522e566e47b161f465e85637029ec260
h-hashimoto-417 <hitomi.seyomo484@outlook.jp>
2026-01-20 14:17:02 +0900

    add append_csv_no_newline

--- GitHubzScripts_from_mineSStuBs/make_linelevel_file.py
+++ GitHubzScripts_from_mineSStuBs/make_linelevel_file.py
@@ -115,6 +115,23 @@ def append_csv(file_path, file_name, data):
         with open(f'{file_path}{file_name}', "a", encoding="utf-8") as f:   # 改行を追加
             f.write("\n")
         data.to_csv(f'{file_path}{file_name}', mode='a', header=False, index=False, encoding="utf-8")
+        
+        
+def append_csv_no_newline(file_path, file_name, data):
+    """
+    Append result into f{file_path}{file_name} without adding a new line before appending.
+    :param file_path: The file location
+    :param file_name: The file name
+    :param data: The data # dataはpandasのDataFrame型を想定
+    :return:
+    """
+    make_path(file_path)    
+    if not os.path.isfile(f'{file_path}{file_name}'):
+        # ファイルが存在しない場合は新規作成
+        data.to_csv(f'{file_path}{file_name}', index=False, encoding="utf-8")
+    else:
+        # ファイルが存在する場合は追記モードで保存（ヘッダーは不要）
+        data.to_csv(f'{file_path}{file_name}', mode='a', header=False, index=False, encoding="utf-8")
 
 
 def make_path(path):
